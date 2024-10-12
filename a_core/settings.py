be30f54cddce79a150d800b83df1adb963caf1b9
olaife219 <durodolaabdulhameed2021@gmail.com>
2024-10-12 06:50:19 -0700

    REALTIME_CHAT

--- a_core/settings.py
+++ a_core/settings.py
@@ -188,10 +188,16 @@ MEDIA_URL = 'media/'
 if ENVIRONMENT == 'development':
     MEDIA_ROOT = BASE_DIR / 'media' 
 else:
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-    CLOUDINARY_STORAGE = {
-        'CLOUDINARY_URL': env('CLOUDINARY_URL')
+    # Check for CLOUDINARY_URL in environment variables
+    CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
+    
+    if CLOUDINARY_URL:
+        DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+        CLOUDINARY_STORAGE = {
+            'CLOUDINARY_URL': CLOUDINARY_URL
         }
+    else:
+        MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
