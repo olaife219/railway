e2f75c7371cdd0b7c2cc62964b01bd57582d7bd6
olaife219 <138580367+olaife219@users.noreply.github.com>
2024-11-18 00:18:15 +0100

    Update models.py

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 
 class ChatGroup(models.Model):
-    group_name = models.CharField(max_length=128, unique=True, default=shortuuid.uuid)
+    group_name = models.CharField(max_length=128, unique=True, blank=True)
     groupchat_name = models.CharField(max_length=128, null=True, blank=True)
     admin = models.ForeignKey(User, related_name='groupchats', blank=True, null=True, on_delete=models.SET_NULL)
     users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True)
@@ -15,12 +15,16 @@ class ChatGroup(models.Model):
     
     def __str__(self):
         return self.group_name
+    def save(self, *args, **kwargs):
+        if not self.group_name:
+            self.group_name = shortuuid.uuid()
+        super().save(*args, **kwargs)
     
 class GroupMessage(models.Model):
     group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.CharField(max_length=300, blank=True, null=True)
-    file = CloudinaryField('files', blank=True, null=True)
+    file = models.FileField(upload_to='files/', blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
 
     @property
